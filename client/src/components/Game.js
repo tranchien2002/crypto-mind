bc94ffaa43120f2ff51ff5bb529ad6855626c5cb
tran.minh.chien <SUN-ASTERISK\tran.minh.chien@b121716-pc.sun-asterisk.com>
2020-03-06 13:56:37 +0700

    [Refs][Task][#454818] Render question to Image

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Row, Col, Statistic, Button, Layout } from 'antd';
-
+import domtoimage from 'dom-to-image';
+import './Game.css';
 const { Content } = Layout;
 const { Countdown } = Statistic;
 
 function Game({ isAnswer, targetTime, onFinish, checkAns, question }) {
   const abcd = ['A', 'B', 'C', 'D'];
   const gameStatus = useSelector((state) => state.gameStatus);
-
+  let ques = document.getElementById('question');
+  domtoimage.toSvg(ques).then((url) => {
+    let quesCol = document.getElementById('quesCol');
+    if (quesCol && quesCol.firstChild) {
+      quesCol.removeChild(quesCol.firstChild);
+    }
+    let img = new Image();
+    img.src = url;
+    if (quesCol) quesCol.appendChild(img);
+  });
   return (
     <Content>
       <Row gutter={[0, 16]} type='flex' justify='center'>
@@ -21,10 +31,10 @@ function Game({ isAnswer, targetTime, onFinish, checkAns, question }) {
         </Col>
         {question[gameStatus.currentQues] ? (
           <Row gutter={[0, 16]} type='flex' justify='center'>
-            <Col span={22}>
-              <p>{question[gameStatus.currentQues].ques}</p>
-            </Col>
-
+            <div id='quesDiv'>
+              <p id='question'>{question[gameStatus.currentQues].ques}</p>
+            </div>
+            <Col span={22} id='quesCol'></Col>
             {question[gameStatus.currentQues].ans.map((ans, index) => (
               <Col span={16} key={index}>
                 {isAnswer ? (
